bf430c29776333ef63f1ea755416b985b4095126
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2023-12-19 16:49:53 +0530

    Add files via upload

---
+++ Test Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test Project")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25076150-a3e5-4d70-b8eb-8d3b77568eb4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test Project/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test_Project
+public class CustomerServiceTests
+{
+    [Test]
+    public void TestCustomerAuthentication_WithInvalidCredentials()
+    {
+        // Arrange
+        ICustomerService customerService = new CustomerService(/* pass your dependencies here */);
+
+        // Act and Assert
+        Assert.Throws<AuthenticationException>(() =>
+        {
+            customerService.Authenticate("invalidUsername", "invalidPassword");
+        });
+    }
+
+    [Test]
+    public void TestUpdateCustomerInformation()
+    {
+        // Arrange
+        ICustomerService customerService = new CustomerService(/* pass your dependencies here */);
+        Customer existingCustomer = /* create an existing customer */;
+        Customer updatedCustomer = /* create an updated customer */;
+
+        // Act
+        customerService.UpdateCustomer(updatedCustomer);
+
+        // Assert
+        Customer retrievedCustomer = customerService.GetCustomerById(existingCustomer.CustomerID);
+        Assert.AreEqual(updatedCustomer.FirstName, retrievedCustomer.FirstName);
+        Assert.AreEqual(updatedCustomer.LastName, retrievedCustomer.LastName);
+        // Add more assertions based on your implementation
+    }
+}
+
